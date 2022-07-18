1eaafbf69460294e46ef5c8d9f9bd377500b7a1a
Danu Widatama <widatama@gmail.com>
2022-07-19 01:30:59 +0700

    Set up ts with esbuild

---
+++ src/exp.ts
@@ -0,0 +1,1 @@
+export const ble = 1;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { ble } from './exp';
+
+export const add = (a: number, b: number): number => a + b;
+export const addBle = (a: number): number => a + ble;
